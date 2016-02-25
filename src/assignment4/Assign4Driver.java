5a700b16ed873e57d5ac23cb24cddc00cbe02d08
kassandrasmith <kassandrasmith@utexas.edu>
2016-02-25 14:17:54 -0600

    Add code stubs and dictionary

---
+++ src/assignment4/Assign4Driver.java
@@ -0,0 +1,30 @@
+package assignment4;
+
+import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class Assign4Driver
+{
+    public static void main(String[] args)
+    {
+        // Create a word ladder solver object
+        Assignment4Interface wordLadderSolver = new WordLadderSolver();
+
+        try 
+        {
+            List<String> result = wordLadderSolver.computeLadder("money", "honey");
+            boolean correct = wordLadderSolver.validateResult("money", "honey", result);
+        } 
+        catch (NoSuchLadderException e) 
+        {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
